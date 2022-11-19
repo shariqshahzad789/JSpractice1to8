20288d5aa8acdbd92d9403085572ec3d05e8057f
shariqshahzad789 <shariqshahzad789@gmail.com>
2022-11-19 12:18:06 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+// var myName ="shairq shahzad";
+// var firstName = "Shariq";
+// var lastName = "Shahzad";
+
+// // var userName = "shariq";
+
+// // var thankMessage = "thank you for order";
+
+// // alert (myName);
+
+// // alert ("my name is " + myName);
+
+// alert ("My Name Is "+ firstName + " " + lastName);
+
+// console.log("my name is "+ firstName + " " + lastName);
+
+// // alert (thankMessage + " " + userName);
+// // console.log(thankMessage + " " + userName);
+
+// // var num1=5;
+// // var num2=6;
+// // var num3="7";
+
+// // var sum = num1 + num2;
+// // var sum = num1 - num2;
+// // var sum = num1 + num2 + num3;
+// // var sum = num1 + num2 - num3;
+
+// // console.log(sum);
+
+// // var originalPrice = 500;
+// // var originalPrice = 600;
+// // var discount = 10;
+// // var discount = 20;
+// // var discountPrice = (discount*originalPrice)/100;
+
+// // var netPrice = originalPrice-discountPrice;
+
+// // console.log( netPrice );
+
+// // var rose = "Rose";
+// // console.log(rose);
+
+// // var one ="2";
+// // var two ="2";
+
+// // var result = one + two 
+// // console.log ("result =>",result);
+
+// // var num = 2;
+// // num = ++num;
+// // num = num++;
+
+// // console.log ("num =>",num);
+
+// // var num = 1;
+
+// // // var newNum = ++num;
+// // // var newNum = num++;
+// // // var newNum = --num
+// // // var newNum = num--;
+
+// // console.log(newNum)
+
+
+for (i = 0; i < 10000; i++) {
+    document.write(i, "<br/>");
+    console.log(i);
+}
